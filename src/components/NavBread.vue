1cd9928548b7d1d2c8e8fb0e23bcf1a391c1756a
gjw <122486975@qq.com>
2017-12-01 18:05:47 +0800

    first commit

---
+++ src/components/NavBread.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+                <a href="/">Home</a>
+                <slot></slot>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+   export default {
+       data () {
+           return {
+
+           }
+       }
+   }
+</script>
+
+<style scoped>
+ 
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+
+import './assets/css/base.css'
+
+Vue.prototype.$ajax = axios
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+})
